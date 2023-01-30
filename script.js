a8a753b9acf0c1f8601beeac46c295ce779be3ba
Maksim-Rudnev <m.rudnev@nv.dunice.net>
2023-01-30 10:01:43 +0300

    fix_formatted

--- script.js
+++ script.js
@@ -1,80 +1,96 @@
-function breakTextIntoChunks() {
-  const input = document.querySelector("#input");
-  const text = input.value;
+const TEST_TEXT = "Lorem ipsum dolor sit amet consectetur adipiscing elit Nullam eleifend odio at magna pretium suscipit Nam commodo mauris felis ut suscipit velit efficitur eget Sed sit amet posuere risus";
+
+function breakTextIntoChunks(text) {
   const limit = 140;
+  const countChunks = 9999;
 
   function getNumberDigit(number) {
     return String(number).length;
   }
 
-  function getTextLengthWithSpaceAndSlash(string) {
-    return  Math.ceil((string.length / (limit - 2)) * limit);
+  function getTextLengthWithSeparators(string) {
+    return Math.ceil((string.length / (limit - 2)) * limit);
   }
 
-  function getLengthWithNextSuffix(length, previousChank = 0, nextChank = '') {
-    if(!previousChank) previousChank = Math.ceil(length/limit);
-    length += previousChank * Math.abs(getNumberDigit(nextChank) - (getNumberDigit(previousChank)));
-    nextChank = Math.ceil(length/limit);
-    if (getNumberDigit(previousChank) !== getNumberDigit(nextChank)) length = getLengthWithNextSuffix(length, nextChank, previousChank);
+  function getLengthWithNextSuffix(length, previousChunk = 0, nextChunk = '') {
+    if (!previousChunk) {
+      previousChunk = Math.ceil(length / limit);
+    }
+    length += previousChunk * Math.abs(getNumberDigit(nextChunk) - (getNumberDigit(previousChunk)));
+    nextChunk = Math.ceil(length / limit);
+    if (getNumberDigit(previousChunk) !== getNumberDigit(nextChunk)) {
+      length = getLengthWithNextSuffix(length, nextChunk, previousChunk);
+    }
     return length;
   }
 
-  function getLengthWithPreviousSuffix(chank) {
+  function getLengthWithPreviousSuffix(chunk) {
     let additionalLength = 0;
-    const getNumberDigitChank = getNumberDigit(chank);
-    for(let rank = 1; rank <= getNumberDigitChank; rank++) {
-      for(let i = 1; i <= chank; i++) {
-        if(getNumberDigit(i) === rank) additionalLength += rank;
+    const getNumberDigitChunk = getNumberDigit(chunk);
+    for (let rank = 1; rank <= getNumberDigitChunk; rank++) {
+      for (let i = 1; i <= chunk; i++) {
+        if (getNumberDigit(i) === rank) {
+          additionalLength += rank;
+        }
       }
     }
     return additionalLength;
   }
 
-  if(text.trim().length) {
-    
-    const getTextLengthWithSpaceAndSlashRezult = getTextLengthWithSpaceAndSlash(text);
-    const getLengthWithNextSuffixRezult = getLengthWithNextSuffix(getTextLengthWithSpaceAndSlashRezult);
-    const getLengthWithPreviousSuffixRezult = getLengthWithPreviousSuffix(Math.ceil(getLengthWithNextSuffixRezult / limit));
-    let estimatedNumberChank =  (Math.ceil((getLengthWithNextSuffixRezult + getLengthWithPreviousSuffixRezult) / limit));
-    let arrayChanks = [];
+  if (text.trim().length) {
+    const textLengthWithSeparators = getTextLengthWithSeparators(text);
+    const lengthWithNextSuffix = getLengthWithNextSuffix(textLengthWithSeparators);
+    const lengthWithPreviousSuffix = getLengthWithPreviousSuffix(Math.ceil(lengthWithNextSuffix / limit));
+    let estimatedNumberChunk = Math.ceil((lengthWithNextSuffix + lengthWithPreviousSuffix) / limit);
+    let arrayChunks = [];
     let arrayWord = text.split(' ');
 
-    if(arrayWord.length === 1 && text.length > 136) estimatedNumberChank = 1;
-    if(estimatedNumberChank === 1) {
-      arrayChanks.push(arrayWord.join(' '));
+    if (arrayWord.length === 1 && text.length > 136) {
+      estimatedNumberChunk = 1;
+    }
+    if (estimatedNumberChunk === 1) {
+      arrayChunks.push(arrayWord.join(' '));
     }
     else {
-      while(true) {
+      while (true) {
         arrayWord = text.split(' ').reverse();
-        let k = 1;
-        arrayChanks = [];
-        while(arrayWord.length) {
-      
-          let limitContent = limit - 1 -getNumberDigit(k) - getNumberDigit(estimatedNumberChank);
+        let indexChunk = 1;
+        arrayChunks = [];
+        while (arrayWord.length) {
+          let limitContent = limit - 1 - getNumberDigit(indexChunk) - getNumberDigit(estimatedNumberChunk);
           let tempStr = '';
-          
-          while(arrayWord.length && limitContent > tempStr.length + arrayWord.at(-1).length) tempStr += arrayWord.pop() + ' ';
-          tempStr += `${k}/${estimatedNumberChank}`;
-          arrayChanks.push(tempStr);
-          k += 1;
+
+          while (arrayWord.length && limitContent > tempStr.length + arrayWord.at(-1).length) {
+            tempStr += arrayWord.pop() + ' ';
+          }
+          tempStr += `${indexChunk}/${estimatedNumberChunk}`;
+          arrayChunks.push(tempStr);
+          indexChunk += 1;
         }
-      
-        if(arrayChanks.length > 9999) {
-          console.log('Превышенно допустимое количество фрагментов!');
+
+        if (arrayChunks.length > countChunks) {
+          console.log("Number of fragments exceeded!");
           break;
-        } 
-        else if (arrayChanks.length === estimatedNumberChank) break;
-        else if (getNumberDigit(arrayChanks.length) === getNumberDigit(estimatedNumberChank)) {
-          arrayChanks = arrayChanks.map((item) => item.replace(estimatedNumberChank,arrayChanks.length));
+        } else if (arrayChunks.length === estimatedNumberChunk) {
           break;
-        } 
-        else estimatedNumberChank = arrayChanks.length;
+        } else if (getNumberDigit(arrayChunks.length) === getNumberDigit(estimatedNumberChunk)) {
+          arrayChunks = arrayChunks.map((item) => item.replace(estimatedNumberChunk, arrayChunks.length));
+          break;
+        } else {
+          estimatedNumberChunk = arrayChunks.length;
+        }
       }
     }
 
-    if(arrayChanks.length <= 9999) console.log(arrayChanks);
-    if(arrayChanks.every((item) => item.length > limit)) console.log('Превышен лимит объема фрагмента!');
-  } else console.log('Вы ничего не ввели.');
+    if (arrayChunks.length <= countChunks) {
+      console.log(arrayChunks);
+    }
+    if (arrayChunks.every((item) => item.length > limit)) {
+      console.log("Fragment size limit exceeded!");
+    }
+  } else {
+    console.log("You didn't enter anything.");
+  }
 }
 
-breakTextIntoChunks();
+breakTextIntoChunks(TEST_TEXT);
